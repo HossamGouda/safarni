606eb7afb8fbb8a9684afea40a8baa57472b7781
Hossam Gouda <hos.gouda90@gmail.com>
2024-01-14 10:01:14 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "normalize.css";
+import "jquery/dist/jquery.js";
+import "popper.js/dist/popper.min";
+import "@popperjs/core";
+window.bootstrap = require("bootstrap/dist/js/bootstrap.js");
+import "bootstrap/scss/bootstrap.scss";
+import "bootstrap/dist/css/bootstrap.rtl.css";
+import "@fortawesome/fontawesome-free/js/all";
+import "./assets/sass/styles.scss";
+import $ from "jquery";
+
+
---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    hot: false,
+    port: 8000,
+    open: false,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          minimize: true,
+        },
+      },
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /custom\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /custom\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "rtlcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./assets/images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./assets/fonts/[name][ext]",
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/style.css",
+    }),
+    new CssMinimizerPlugin(),
+  ],
+};
